896ccafc63c25e4bd3cf8d70a55a97f0f1f59745
Md Tauqueer manzar <141390259+Tauqueer123manzar@users.noreply.github.com>
2024-01-18 23:32:18 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,44 @@
+let btn=document.querySelector("button");
+let inputs=document.querySelector("input");
+let userInput=document.getElementById("date");
+let result=document.getElementById("current-age");
+userInput.max =new Date().toISOString().split("T")[0];
+btn.addEventListener("click",function(){
+    let birthdate=new Date(userInput.value);
+
+    let d1= birthdate.getDate();
+    let m1= birthdate.getMonth();
+    let y1= birthdate.getFullYear();
+
+    let today= new Date();
+
+    let d2= today.getDate();
+    let m2= today.getMonth();
+    let y2= today.getFullYear();
+
+    let m3,y3,d3;
+
+    y3=y2-y1;
+    if(m2>=m1){
+        m3=m2-m1;
+    }else{
+       y3--;
+       m3= 12+m2-m1;   
+    }
+
+    if(d2>=d1){
+        d3=d2-d1;
+    }else{
+        m3--;
+        d3=getDaysInmonth(y1,m1)+d2-d1;
+    }
+    if(m3<0){
+        m3=11;
+        y3--;
+    }
+    result.innerHTML=`You are ${y3} Years , ${m3} months , ${d3} Days Old`;
+
+});
+function getDaysInmonth(year,month){
+    return new Date(year,month,0).getDate();
+}
